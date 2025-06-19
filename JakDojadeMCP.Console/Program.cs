2a8a115abef1efec69806133007118d6d189a918
Jakub Kuniszewski <you@example.com>
2025-06-19 18:41:36 +0200

    Add HTTP client test program

---
+++ JakDojadeMCP.Console/Program.cs
@@ -0,0 +1,50 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using JakDojadeMCP.Server.Clients;
+using JakDojadeMCP.Server.Models;
+
+var client = new JakDojadeClient(new HttpClient
+{
+    BaseAddress = new Uri("https://jakdojade.pl")
+});
+
+
+var cities = await client.GetCitiesAsync();
+var citiesList = cities.ToList();
+
+var schedules = await client.GetScheduleTableAsync(1, "7", "ROSR-01");
+
+var locations = await client.GetLocationsAsync("POZNAN", "rondo");
+var locationsList = locations.ToList();
+
+
+var routes = await client.GetRoutesAsync(
+    new FindRoute(
+        new SimpleCoordinate("52.23232","21.01599"),
+        new SimpleCoordinate("52.26289", "20.98983"),
+        null,
+        null,
+        null,
+        null,
+        1,
+        1,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null
+        )
+    );
+
+var routesList = routes.ToList();
+
+
+Console.WriteLine("END");
