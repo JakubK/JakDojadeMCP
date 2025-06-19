e3c6901f30f2fada6e78fc514899593d0c004b50
Jakub Kuniszewski <you@example.com>
2025-06-19 16:08:30 +0200

    Add boilerplate code for MCP server

---
+++ JakDojadeMCP.Server/Program.cs
@@ -0,0 +1,15 @@
+﻿using JakDojadeMCP.Server.Clients;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var builder = Host.CreateApplicationBuilder(args);
+
+builder.Services.AddMcpServer()
+    .WithStdioServerTransport();
+    
+builder.Services.AddHttpClient<JakDojadeClient>(x =>
+{
+    x.BaseAddress = new Uri("https://jakdojade.pl");
+});
+
+await builder.Build().RunAsync();
