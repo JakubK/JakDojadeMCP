69beba85acf860308d2f112038481b832dbeb6f8
Jakub Kuniszewski <you@example.com>
2025-06-19 16:39:54 +0200

    Add optional basic auth setup

--- JakDojadeMCP.Server/Program.cs
+++ JakDojadeMCP.Server/Program.cs
@@ -1,4 +1,6 @@
-﻿using JakDojadeMCP.Server.Clients;
+﻿using System.Net.Http.Headers;
+using System.Text;
+using JakDojadeMCP.Server.Clients;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -6,10 +8,19 @@ var builder = Host.CreateApplicationBuilder(args);
 
 builder.Services.AddMcpServer()
     .WithStdioServerTransport();
-    
+
+string authToken = string.Empty;
+
+if (args.Length >= 2) {
+    var publicKey = args[0];
+    var secretKey = args[1];
+    authToken = Convert.ToBase64String(Encoding.ASCII.GetBytes($"{publicKey}:{secretKey}"));
+}
+
 builder.Services.AddHttpClient<JakDojadeClient>(x =>
 {
     x.BaseAddress = new Uri("https://jakdojade.pl");
+    x.DefaultRequestHeaders.Authorization = string.IsNullOrEmpty(authToken) ? null : new AuthenticationHeaderValue("Basic", authToken);
 });
 
 await builder.Build().RunAsync();
