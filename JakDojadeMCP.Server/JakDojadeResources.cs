079fec856ce3883b85b110809244c890223f3ebc
Jakub Kuniszewski <you@example.com>
2025-06-20 16:42:37 +0200

    Update resource

--- JakDojadeMCP.Server/JakDojadeResources.cs
+++ JakDojadeMCP.Server/JakDojadeResources.cs
@@ -1,6 +1,7 @@
 ﻿using System.ComponentModel;
 using System.Text.Json;
 using JakDojadeMCP.Server.Clients;
+using ModelContextProtocol.Protocol;
 using ModelContextProtocol.Server;
 
 namespace JakDojadeMCP.Server;
@@ -8,10 +9,15 @@ namespace JakDojadeMCP.Server;
 [McpServerResourceType]
 public class JakDojadeResources
 {
-    [McpServerResource(Name = "List Cities and their data", MimeType = "text/plain"), Description("List all cities and their data such as operators and agglomeration names identified by normalizedName")]
-    public static async Task<string> ListCitiesAsync(JakDojadeClient client)
+    [McpServerResource(UriTemplate = "https://jakdojade.pl/api/rest/v1/cities", MimeType = "text/plain"), Description("List all cities and their data such as operators and agglomeration names identified by normalizedName")]
+    public static async Task<ResourceContents> ListCitiesAsync(JakDojadeClient client)
     {
         var cities = await client.GetCitiesAsync();
-        return JsonSerializer.Serialize(cities);
+        return new TextResourceContents
+        {
+            Text = JsonSerializer.Serialize(cities),
+            MimeType = "text/plain",
+            Uri = "https://jakdojade.pl/api/rest/v1/cities",
+        };
     }
 }
